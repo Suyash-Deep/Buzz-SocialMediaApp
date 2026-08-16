7cfe4c0e185061d5d7518dd4833554b20262dc0f
Suyash-Deep <97466907+Suyash-Deep@users.noreply.github.com>
2026-08-16 15:01:13 +0530

    Fix frontend configuration

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 import daisyui from "daisyui";
-import daisyUIThemes from "daisyui/src/theming/themes";
+import daisyUIThemes from "daisyui/src/theming/themes.js";
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
